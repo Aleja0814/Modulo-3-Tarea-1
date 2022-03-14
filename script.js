4724b3595a71414bd004fb89dabc5b009e7844f2
Alejandra Bastidas <ale_basti@hotmail.com>
2022-03-14 13:45:39 -0500

    Tarea1 Modulo3 Corregida

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let calculoPropina;
 let calculoTotal;
 let boton = document.getElementById("boton");
+let form1 =document.querySelector("form")
 let totalCnta = document.querySelector("h3");
 let propina = document.querySelector("h4");
 let totalPagar = document.querySelector("h5");
 
 
-boton.addEventListener("click", function () {
-   
+boton.addEventListener("click", function (e) {
+   e.preventDefault()
     let cuenta = document.getElementById("totalcuenta").value;
     let porcentaje = document.getElementById("porcentajepropina").value;
+    
     calculoPropina = cuenta*(porcentaje/100);
     calculoTotal = parseFloat(cuenta)+parseFloat(calculoPropina);
 
     totalCnta.textContent = cuenta;
     propina.textContent = calculoPropina;
     totalPagar.textContent = calculoTotal;
+    form1.reset()
+    
 })
